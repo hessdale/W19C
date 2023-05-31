1f0601669f6fb7243cf600c46e4514595d181dba
hessdale <hessdale96@gmail.com>
2023-05-30 23:34:18 -0600

    working on importing the att and def

---
+++ app.py
@@ -0,0 +1,16 @@
+import input_handler
+import random
+
+
+
+
+def game():
+    attack=input_handler.get_attack()
+    defence=input_handler.get_defence()
+    cpu_attack= random.randint(1,2)
+    cpu_defence= random.randint(1,2)
+    
+    
+
+
+game()
---
+++ input_handler.py
@@ -0,0 +1,9 @@
+def get_attack():
+    print('Punch:1 Kick:2')
+    attack=input('enter attack: ')
+    return(int(attack))
+
+def get_defence():
+    print('High:1 Low:2')
+    defence=input('enter defence: ')
+    return(int(defence))
