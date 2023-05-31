cd012bd628f4da91273480cad40412195011afd5
hessdale <hessdale96@gmail.com>
2023-05-31 13:17:30 -0600

    finished commenting code

--- app.py
+++ app.py
@@ -1,16 +1,48 @@
+# import input handler to take inputs from user
 import input_handler
+# import random to generate random nums
 import random
-
-
-
-
-def game():
-    attack=input_handler.get_attack()
-    defence=input_handler.get_defence()
-    cpu_attack= random.randint(1,2)
-    cpu_defence= random.randint(1,2)
-    
-    
-
-
-game()
+# defining game function with health arguements
+def game(user_health,cpu_health):
+    # returned strings from input handler
+    user_attack=input_handler.get_attack()
+    user_defence=input_handler.get_defence()
+    # random numbers generated from 0 - 100 for cpu
+    cpu_attack= random.randint(0,100)
+    cpu_defence= random.randint(0,100)
+    # if statements that have all the out comes of the battle
+    if (user_attack == 1 and cpu_defence >= 50):
+        cpu_health=cpu_health - 10
+        print('you hit the cpu!')
+        print('cpu hp:')
+        print(cpu_health)
+    elif(user_attack == 1 and cpu_defence < 50):
+        print('cpu attack blocked')
+    elif(user_attack == 2 and cpu_defence >= 50):
+        print('cpu attack blocked')
+    elif(user_attack == 2 and cpu_defence < 50):
+        cpu_health=cpu_health - 10
+        print('you hit the cpu!')
+        print('cpu hp:')
+        print(cpu_health)
+    if (cpu_attack < 50 and user_defence ==2):
+        user_health=user_health - 10
+        print('your hp:')
+        print(user_health)
+    elif (cpu_attack < 50 and user_defence == 1):
+        print('you blocked the attack')
+    elif (cpu_attack >= 50 and user_defence == 2):
+        print('you blocked the attack')
+    elif (cpu_attack >= 50 and user_defence == 1):
+        user_health=user_health - 10
+        print('your hp:')
+        print(user_health)
+    # if statement at end of fight to check health and if its above 0 then it re runs the function with health as arguements
+    if(cpu_health > 0 and user_health > 0):
+        game(cpu_health,user_health)
+    elif(cpu_health <= 0):
+        print('you have won!')
+    elif(user_health <= 0):
+        print('you have lost!')
+# starting the game with players health at 100
+game(100,100)
--- input_handler.py
+++ input_handler.py
@@ -1,9 +1,30 @@
+# functon that tries to get attack input from user and returns errors and restarts the function
 def get_attack():
     print('Punch:1 Kick:2')
-    attack=input('enter attack: ')
-    return(int(attack))
-
+    try:
+        attack=input('enter attack: ')
+        return(int(attack))
+    except ValueError:
+        print('please enter number 1-2')
+        get_attack()
+    except TypeError:
+        print('only select 1-2')
+        get_attack()
+    except:
+        print('something went wrong')
+        get_attack()
+# functon that tries to get defence input from user and returns errors and restarts the function
 def get_defence():
     print('High:1 Low:2')
-    defence=input('enter defence: ')
-    return(int(defence))
+    try:
+        defence=input('enter defence: ')
+        return(int(defence))
+    except ValueError:
+        print('please enter number 1-2')
+        get_defence()
+    except TypeError:
+        print('only select 1-2')
+        get_defence()
+    except:
+        print('something went wrong')
+        get_defence()
